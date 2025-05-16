6836707fe00229060c21018b880e23358dbedecb
stelzo <stelzo@steado.de>
2025-05-16 15:02:17 +0200

    no async needed here

--- ratpubsub/examples/sub.rs
+++ ratpubsub/examples/sub.rs
@@ -5,7 +5,7 @@ use ros2_interfaces_jazzy_rkyv::std_msgs::msg;
 async fn main() -> anyhow::Result<()> {
     env_logger::init();
     let node = Node::create("subber").await?;
-    let mut rx = node.subscribe::<msg::String>("some_topic", 10).await?;
+    let mut rx = node.subscribe::<msg::String>("some_topic", 10)?;
 
     while let Some(msg) = rx.recv().await {
         println!("Received Message: {}", &msg.data);
